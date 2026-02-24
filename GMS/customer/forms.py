032b134593097a322ac221399a8d3f76e5c9b904
SAMPADAHAH <parajulisampada57@gmail.com>
2026-02-24 12:26:43 +0545

    user email verification

--- GMS/customer/forms.py
+++ GMS/customer/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from .models import *
-# User = get_user_model()
+User = get_user_model()
 
 class SignUpForm(forms.ModelForm):
     password1 = forms.CharField(
